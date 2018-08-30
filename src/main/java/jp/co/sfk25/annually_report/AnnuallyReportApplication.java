2c07973467fffeb8d3711d611f5742c86eec74fa
tsu-kaneko <gamuuma@gmail.com>
2018-08-30 17:07:37 +0900

    first commit

---
+++ src/main/java/jp/co/sfk25/annually_report/AnnuallyReportApplication.java
@@ -0,0 +1,12 @@
+package jp.co.sfk25.annually_report;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnuallyReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnnuallyReportApplication.class, args);
+	}
+}
---
+++ src/test/java/jp/co/sfk25/annually_report/AnnuallyReportApplicationTests.java
@@ -0,0 +1,16 @@
+package jp.co.sfk25.annually_report;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AnnuallyReportApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
