ca3669c28063aaf274d11ade24d0fa63e665901f
fukada <ps.ev.si.nc.mr.sw@gmail.com>
2018-09-15 15:34:53 +0900

    ユーザ一覧取得api追加

--- src/main/java/jp/co/sfk25/annually_report/IndexController.java
+++ src/main/java/jp/co/sfk25/annually_report/IndexController.java
@@ -6,9 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
-    @RequestMapping(value="/")
-    public String index(){
+    @RequestMapping(value = "/")
+    public String index() {
         return "index";
     }
-
 }
--- src/main/java/jp/co/sfk25/annually_report/domain/repository/UserRepository.java
+++ src/main/java/jp/co/sfk25/annually_report/domain/repository/UserRepository.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.jooq.DSLContext;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 import static jp.co.sfk25.annually_report.jooq.tables.Users.USERS;
 
 /**
@@ -15,19 +17,23 @@ import static jp.co.sfk25.annually_report.jooq.tables.Users.USERS;
 @RequiredArgsConstructor
 public class UserRepository {
 
-  private final DSLContext dslContext;
+    private final DSLContext dslContext;
 
-  public User findOne(int id) {
-    return dslContext.selectFrom(USERS).where(USERS.ID.eq(id)).fetchOne(this::toEntity);
-  }
+    public User findOne(int id) {
+        return dslContext.selectFrom(USERS).where(USERS.ID.eq(id)).fetchOne(this::toEntity);
+    }
 
-  private User toEntity(UsersRecord record) {
-    return User.of(
-            record.getId(),
-            record.getName(),
-            // TODO パスワード復号化処理
-            record.getPassword(),
-            record.getCreatedAt().toLocalDateTime(),
-            record.getUpdatedAt().toLocalDateTime());
-  }
+    public List<User> findAll() {
+        return dslContext.selectFrom(USERS).fetch(this::toEntity);
+    }
+
+    private User toEntity(UsersRecord record) {
+        return User.of(
+                record.getId(),
+                record.getName(),
+                // TODO パスワード復号化処理
+                record.getPassword(),
+                record.getCreatedAt().toLocalDateTime(),
+                record.getUpdatedAt().toLocalDateTime());
+    }
 }
