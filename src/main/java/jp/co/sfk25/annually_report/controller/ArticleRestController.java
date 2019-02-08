4d92a6d3959a95885d8e24132b18aeeff4233ea8
tsu-kaneko <tsu.kaneko0410@gmail.com>
2019-02-08 11:51:35 +0900

    APIのポート変更 CORSでなぜかエラーが出るためアノテーションで対応

--- src/main/java/jp/co/sfk25/annually_report/controller/ArticleRestController.java
+++ src/main/java/jp/co/sfk25/annually_report/controller/ArticleRestController.java
@@ -5,6 +5,7 @@ import jp.co.sfk25.annually_report.domain.entity.User;
 import jp.co.sfk25.annually_report.service.ArticleService;
 import jp.co.sfk25.annually_report.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/v1/articles")
+@CrossOrigin()
 public class ArticleRestController {
     private final ArticleService articleService;
 
