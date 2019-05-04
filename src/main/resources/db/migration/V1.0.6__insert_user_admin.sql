140ccc8852ca5ae21d1850b66ac6788a4fa15ed1
tsu-kaneko <tsu.kaneko0410@gmail.com>
2019-05-04 14:47:50 +0900

    ユーザー、タグ、グループ、	プロセスのテーブルのINSERT文追加

---
+++ src/main/resources/db/migration/V1.0.6__insert_user_admin.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (name, email, password, created_at, updated_at, group_id)
+VALUES ('admin', 'admin@domain.co.jp', '$2a$10$/jgElt3NSxkaTPr8qwwYP.31BcpkGw/KZq46PdsR4YsCCjVfrBFl2', now(), now(), 1);
