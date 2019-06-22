9b4be0bc3245d1afc85e2b821511bcfe3bb005b2
tsu-kaneko <tsu.kaneko0410@gmail.com>
2019-06-22 16:25:02 +0900

    ユーザーテーブルのカラムを変更

--- src/main/resources/db/migration/V1.0.7__alter_users_table.sql
+++ src/main/resources/db/migration/V1.0.7__alter_users_table.sql
@@ -1,8 +1,8 @@
 ALTER TABLE users DROP COLUMN group_id;
 
-ALTER TABLE users ADD group_id int(11) NOT NULL AFTER name;
-ALTER TABLE users ADD self_introduction varchar(64) AFTER email;
-ALTER TABLE users ADD birthday DATETIME AFTER email;
-ALTER TABLE users ADD blood_type int(11) AFTER email;
-ALTER TABLE users ADD sex int(11) AFTER email;
-ALTER TABLE users ADD entering_company_date DATETIME AFTER email;
+ALTER TABLE users ADD group_id int(11) NOT NULL AFTER password;
+ALTER TABLE users ADD self_introduction varchar(64) AFTER password;
+ALTER TABLE users ADD birthday DATETIME AFTER password;
+ALTER TABLE users ADD blood_type int(11) AFTER password;
+ALTER TABLE users ADD sex int(11) AFTER password;
+ALTER TABLE users ADD entering_company_date DATETIME AFTER password;
